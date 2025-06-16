f576471431c98d495ec0cfefe621e80fafc5b2e5
Your Name <you@example.com>
2025-06-16 18:24:08 +0500

    templatesga o'zgarish kiritildi

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from .views import *
 urlpatterns=[
     path('',index_view),
-    path('all-professors/', all_proffesors_view),  
-    path('all-students/', all_students_view),
+    path('all-professors/', all_proffesors_view), 
+    # ustozlar 
+    path('all-students/', all_students_view), 
+    # student oquvchi
     path('add-professor/', add_professor_view),
     path('add-student/', add_student_view),
     path('edit-professor/', edit_professor_view),
@@ -12,6 +14,7 @@ urlpatterns=[
     path('student-profile/', student_profile_view),
     path('add-course/', add_course_view),
     path('all-courses/', all_courses_view),
+    # kurlas
     path('edit-course/', edit_course_view),
     path('course-info/',course_info_view),
     path('course-payment/',course_payment_view),
